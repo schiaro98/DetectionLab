ddc270a48c1437adb5552ebe4dcba6f02a04089a
davide schiaroli <davide.schiaroli2@studio.unibo.it>
2022-10-26 17:23:23 +0200

    Remove objects in restore is no more commented
    Signed-off-by: davide schiaroli <davide.schiaroli2@studio.unibo.it>

--- Vagrant/scripts/restore-ad.ps1
+++ Vagrant/scripts/restore-ad.ps1
@@ -2,7 +2,7 @@ $users = Get-Content users.txt
 
 foreach ($user in (Get-ADUser -Filter *).samAccountName){
     if($users.Contains($user) -eq $false){
-        #Remove-ADUser -Identity $user -Confirm:$false
+        Remove-ADUser -Identity $user -Confirm:$false
         Write-Host "Removed $user from Active Directory users"
     }
 }
@@ -11,7 +11,7 @@ $workstations = Get-Content computers.txt
 
 foreach ($pc in (Get-ADComputer -Filter *).samAccountName){
     if($workstations -notcontains $pc){
-        #Remove-ADComputer -Identity $pc -Confirm:$false
+        Remove-ADComputer -Identity $pc -Confirm:$false
         Write-Host "Removed $pc from Active Directory computers"
     }
 }
@@ -20,7 +20,7 @@ $groups = Get-Content groups.txt
 
 foreach ($group in (Get-ADGroup -Filter *).samAccountName){
     if($groups -notcontains $group){
-        #Remove-ADGroup -Identity $group -Confirm:$false
+        Remove-ADGroup -Identity $group -Confirm:$false
         Write-Host "Removed $group from Active Directory groups"
     }
 }
@@ -29,7 +29,7 @@ $ous = Get-Content ous.txt
 
 foreach ($ou in (Get-ADOrganizationalUnit -Filter *).Name){
     if($ous -notcontains $ou){
-        #Remove-ADOrganizationalUnit -Identity $ou -Confirm:$false
+        Remove-ADOrganizationalUnit -Identity $ou -Confirm:$false
         Write-Host "Removed $ou from Active Directory Organizational Units"
     }
 }
