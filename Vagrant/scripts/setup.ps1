5315eed8de937e00342d7667aaf354a09a378248
davide schiaroli <davide.schiaroli2@studio.unibo.it>
2022-10-27 09:25:03 +0200

    Setup for language and alias setting

---
+++ Vagrant/scripts/setup.ps1
@@ -0,0 +1,6 @@
+Set-WinUserLanguageList -LanguageList it-IT -force
+Set-Alias mz C:\Tools\Mimikatz\x64\mimikatz.exe
+Set-Alias mimikatz C:\Tools\Mimikatz\x64\mimikatz.exe
+Set-Alias Rubeus C:\Tools\Rubeus.exe
+Set-Alias hashcat C:\Tools\hashcat-6.2.6\hashcat.exe
+Set-Alias psexec C:\Tools\PSTools\PsExec.exe
