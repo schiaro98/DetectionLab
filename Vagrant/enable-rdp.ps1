e8cdf39ccc0c20f4bc0a440ea73fce4613b37769
davide schiaroli <davide.schiaroli2@studio.unibo.it>
2022-12-09 15:08:32 +0100

    Script for automatically add users to rdp group

---
+++ Vagrant/enable-rdp.ps1
@@ -0,0 +1,9 @@
+$usersDomain = @("domainUser1", "domainUser2", "domainUser3", "domainUser4", "vagrant")
+
+foreach ($user in $usersDomain) {
+    Add-ADGroupMember -Identity "Remote Dekstop Users" -Members $user
+}
+
+net localgroup "Remote Desktop Users" "local1" /add
+net localgroup "Remote Desktop Users" "local2" /add
+net localgroup "Remote Desktop Users" "vagrant" /add
